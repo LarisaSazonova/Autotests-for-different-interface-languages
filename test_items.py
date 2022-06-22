0165f8a4456380bb211b14027b99e6ca3b1f875a
Larisa <ssnewh@gmail.com>
2022-06-22 13:49:31 +0300

    added files

---
+++ test_items.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import time
+
+def test_add_to_basket_button_available(browser):
+    
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    time.sleep(10)
+
+    elements = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")
+
+    assert elements is not None
+
